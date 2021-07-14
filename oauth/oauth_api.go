7aabb4bb7d429bb68ed77aee906b790ac7d15142
nicoleta.fratila <nicoleta.fratila@softvision.com>
2021-07-14 11:41:38 +0300

    use common errors library

--- oauth/oauth_api.go
+++ oauth/oauth_api.go
@@ -62,7 +62,7 @@ func GetClientId(request *http.Request) int64 {
 	return clientId
 }
 
-func AuthenticateRequest(request *http.Request) *rest_errors.RestErr {
+func AuthenticateRequest(request *http.Request) rest_errors.RestErr {
 	if request == nil {
 		return nil
 	}
@@ -76,7 +76,7 @@ func AuthenticateRequest(request *http.Request) *rest_errors.RestErr {
 
 	at, err := getAccessToken(access_tokenId)
 	if err != nil {
-		if err.Status == http.StatusNotFound {
+		if err.Status() == http.StatusNotFound {
 			return nil
 		}
 		return err
@@ -97,7 +97,7 @@ func cleanRequest(request *http.Request) {
 	request.Header.Del(headerXCallerId)
 }
 
-func getAccessToken(accessTokenId string) (*accessToken, *rest_errors.RestErr) {
+func getAccessToken(accessTokenId string) (*accessToken, rest_errors.RestErr) {
 	response := oauthRestClient.Get(fmt.Sprintf("/oauth/access_token/%s", accessTokenId))
 	if response == nil || response.Response == nil {
 		return nil, rest_errors.NewInternalServerError("invalid rest client response when trying to get access token", errors.New("authentication error"))
@@ -108,7 +108,7 @@ func getAccessToken(accessTokenId string) (*accessToken, *rest_errors.RestErr) {
 		if err := json.Unmarshal(response.Bytes(), &restErr); err != nil {
 			return nil, rest_errors.NewInternalServerError("invalid error interface when trying to get access token", errors.New("authentication error"))
 		}
-		return nil, &restErr
+		return nil, restErr
 	}
 
 	var at accessToken
