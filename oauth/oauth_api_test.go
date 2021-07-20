8638fabb64a3ccc5d0e7e3c1d3c32c30cf679604
nicoleta.fratila <nicoleta.fratila@softvision.com>
2021-07-20 15:29:25 +0300

    test

---
+++ oauth/oauth_api_test.go
@@ -0,0 +1,69 @@
+package oauth
+
+import (
+	"fmt"
+	"github.com/mercadolibre/golang-restclient/rest"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("about to start oauth tests")
+	//flag.Bool("mock", true, "Use 'mock' flag to tell package rest that you would like to use mockups.")
+	//flag.Parse()
+	rest.StartMockupServer()
+	os.Exit(m.Run())
+}
+
+func TestOauthConstants(t *testing.T) {
+	assert.EqualValues(t, "X-Public", headerXPublic)
+	assert.EqualValues(t, "X-Client-Id", headerXClientId)
+	assert.EqualValues(t, "X-Caller-Id", headerXCallerId)
+	assert.EqualValues(t, "access_token", paramAccessToken)
+}
+
+func TestIsPublicNilRequest(t *testing.T) {
+	assert.True(t, IsPublic(nil))
+}
+
+func TestIsPublicNoError(t *testing.T) {
+	request := http.Request{
+		Header: make(http.Header),
+	}
+	assert.False(t, IsPublic(&request))
+
+	request.Header.Add("X-Public", "true")
+	assert.True(t, IsPublic(&request))
+}
+
+func TestGetCallerNilRequest(t *testing.T) {
+
+}
+
+func TestGetCallerInvalidCallerFormat(t *testing.T) {
+
+}
+
+func TestGetCallerNoError(t *testing.T) {
+
+}
+
+func TestGetAccessTokenInvalidRestclientResponse(t *testing.T) {
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		HTTPMethod:   http.MethodGet,
+		URL:          "http://localhost:8080/oauth/access_token/abc123",
+		ReqBody:      ``,
+		RespHTTPCode: -1,
+		RespBody:     `{}`,
+	})
+
+	accessToken, err := getAccessToken("abc123")
+	assert.Nil(t, accessToken)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "invalid rest client response when trying to get access token", err.Message())
+
+}
